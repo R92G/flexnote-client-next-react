83ee71acac8ee61a13995ab71ca072133b3ed85a
Robin <robinlaurentius@gmail.com>
2024-05-08 16:13:10 +0200

    Refactor Sponsors component to improve layout and responsiveness

--- app/components/Sponsors.tsx
+++ app/components/Sponsors.tsx
@@ -39,7 +39,7 @@ export const Sponsors = () => {
         Why people are using Flexnote?
       </h2>
 
-      <div className="flex flex-wrap justify-center items-center gap-4 md:gap-8">
+      <div className="flex flex-col lg:flex-row lg:flex-wrap justify-center items-start w-[400px] lg:w-fit mx-auto lg:mx-0 lg:items-center gap-4 md:gap-8">
         {sponsors.map(({ icon, name }: SponsorProps) => (
           <div
             key={name}
