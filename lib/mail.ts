8ad8a44bd16e75f987a6366ef32a46aaf5a06462
Robin <robinlaurentius@gmail.com>
2024-05-13 17:34:26 +0200

    refactor: Update email sender information in mail.ts

--- lib/mail.ts
+++ lib/mail.ts
@@ -8,7 +8,7 @@ const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
+    from: "Flexnote | Authentication <auth@flexnote.io>",
     to: email,
     subject: "2FA Code",
     html: `<p>Your 2FA code: ${token}</p>`,
@@ -19,7 +19,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
+    from: "Flexnote | Authentication <auth@flexnote.io>",
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
@@ -30,7 +30,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
+    from: "Flexnote | Authentication <auth@flexnote.io>",
     to: email,
     subject: "Confirm your email",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
@@ -53,9 +53,9 @@ export const sendScriptTag = async (email: string, websiteId: string) => {
   </pre>`;
 
   await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
+    from: "Flexnote | Flexible Notifications <info@flexnote.io>",
     to: email,
-    subject: "Notification Script Tag",
+    subject: "Flexnote | Script Tag",
     html: `<div><p>Hi,</p><br/><br/><p>Please copy and paste the code at the end of your body tag</p><br/>${code}</div>`,
   });
 };
@@ -71,7 +71,7 @@ export const createResendContact = async (email: string) => {
   });
 
   await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
+    from: "Flexnote | Flexible Notifications <info@flexnote.io>",
     to: "robinlaurentius@gmail.com",
     subject: "NEW DEMO REQUEST",
     html: `<div>NEW DEMO REQUEST BY ${email}</div>`,
