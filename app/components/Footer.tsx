a4ae8949cc64d0e95d603b1f57567f8772fc8f19
Robin <robinlaurentius@gmail.com>
2024-05-14 09:43:09 +0200

    refactor: Remove LogoIcon from Footer component
    The LogoIcon component was removed from the Footer component as it is no longer needed. This change simplifies the code and improves performance.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -35,7 +35,6 @@ export const Footer = () => {
       <section className="container py-20 grid grid-cols-1 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
         <div className="col-span-full xl:col-span-2">
           <a href="/" className="font-bold text-xl flex justify-center">
-            <LogoIcon />
             Flexnote{" "}
           </a>
         </div>
